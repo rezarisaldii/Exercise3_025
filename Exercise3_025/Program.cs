aab8335738905019174cd20d8ea81d5aaf2d5cda
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-11-25 16:30:41 +0700

    Searches for the specified node, return true if the node is found, If the node is present at the end, return false if the node is not found

--- Exercise3_025/Program.cs
+++ Exercise3_025/Program.cs
@@ -22,9 +22,22 @@ namespace Exercise3_025
 
         public Circularlist()
         {
-
+            LAST = null;
         }
 
+        public bool Search(int rollNo, ref Node previous, ref Node current) /*Searches for the specified node*/
+        {
+            for(previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if(rollNo == current.rollNumber)
+                    return (true); /*return true if the node is found*/
+
+            }
+            if (rollNo == LAST.rollNumber) /*If the node is present at the end*/
+                return true;
+            else
+                return (false); /*return false if the node is not found*/
+        }
     }
     internal class Program
     {
