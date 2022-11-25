98e1e0bcf8c75ec1d2d5d9fa86e6de6af4cb2d73
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-11-25 16:05:25 +0700

    Add project files.

---
+++ Exercise3_025/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
