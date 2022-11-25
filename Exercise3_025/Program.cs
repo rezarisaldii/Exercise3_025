a9bf7cb1c4c0aaf53216cd3b6880fca90fb0ab62
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-11-25 16:47:11 +0700

    firstNode

--- Exercise3_025/Program.cs
+++ Exercise3_025/Program.cs
@@ -64,6 +64,13 @@ namespace Exercise3_025
                 Console.Write(LAST.rollNumber + "         " + LAST.name + "\n");
             }
         }
+        public void firstNode()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+                Console.WriteLine("\nThe first record in the list is:\n\n " + LAST.next.rollNumber + "       " + LAST.next.name);
+        }
     }
     internal class Program
     {
